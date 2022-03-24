2ac3d54e98834bac72f470cf705b2a74e23719ef
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-23 23:15:31 -0700

    Made a Player class, and an inner class Hero

---
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Player.java
@@ -0,0 +1,21 @@
+package ca.bcit.comp2522.termproject.valhalla.entities;
+
+/**
+ * A Player class.
+ * @author FonseLULW
+ * @version 1.0
+ */
+public class Player {
+    // this is just the Player (you!)
+
+    /**
+     * A Hero class.
+     * @author FonseLULW
+     * @version 1.0
+     */
+    public class Hero {
+        // this is the PlayerCharacter (the one that moves)
+
+        // Can the Hero (PlayerCharacter) exist without a Player controlling them?????
+    }
+}
