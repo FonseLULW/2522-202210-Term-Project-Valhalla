be1622286002c119c8c8d3b6db429fabe7123f62
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-24 11:10:37 -0700

    Modified constructors

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Hero.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Hero.java
@@ -1,24 +1,19 @@
 package ca.bcit.comp2522.termproject.valhalla.entities;
 
-
-
 public class Hero extends Entity {
     private static final int DEFAULT_X = 50;
     private static final int DEFAULT_Y = 50;
-    private static final int DEFAULT_WIDTH = 150;
-    private static final int DEFAULT_HEIGHT = 150;
 
-    public Hero() {
-        super("baymax.jpg");
+    public Hero(final String filename, final int x, final int y) {
+        super(filename, x, y);
+    }
+
+    public Hero(final String filename) {
+        this(filename, DEFAULT_X, DEFAULT_Y);
     }
 
     @Override
     public void attack() {
-
-    }
-
-    @Override
-    public void move() {
 
     }
 
