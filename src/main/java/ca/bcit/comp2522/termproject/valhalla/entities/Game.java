4c85bf69786509179393e272ac262715c5ee10f6
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-24 11:11:52 -0700

    Started implementing event listeners

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Game.java
@@ -5,27 +5,34 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 
 public class Game extends Application {
-    private static final int APP_WIDTH = 1000;
-    private static final int APP_HEIGHT = 563;
+    public static final int APP_WIDTH = 1000;
+    public static final int APP_HEIGHT = 563;
     private static final String APP_TITLE = "Valhalla";
 
     @Override
     public void start(final Stage stage) {
-        // group
+        // application setup
         Group root = new Group();
         Scene scene = new Scene(root, APP_WIDTH, APP_HEIGHT, Color.GAINSBORO);
+        stage.setTitle(APP_TITLE);
 
-        // game
-        Hero hero = new Hero();
-        hero.setHeight(500);
+        // game setup
+        Hero hero = new Hero("baymax.jpg");
         root.getChildren().add(hero.getSprite());
 
+        // event listeners
+        scene.setOnKeyPressed(this::handleKeyPress);
 
-        stage.setTitle(APP_TITLE);
+        // display the screen
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void handleKeyPress(final KeyEvent event) {
+        System.out.println(event);
     }
 
     /**
