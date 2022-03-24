2518d83a1ea19b1f394aef2fe5b7e90234e56c47
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-23 23:14:14 -0700

    Made an interface called Slayable

---
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Slayable.java
@@ -0,0 +1,19 @@
+package ca.bcit.comp2522.termproject.valhalla.entities;
+
+/**
+ * A Slayable interface for all things that can be slain.
+ * @author FonseLULW
+ * @version 1.0
+ */
+public interface Slayable {
+    /**
+     * Checks whether a Slayable is alive.
+     * @return a boolean true if alive, else false
+     */
+    boolean isAlive();
+
+    /**
+     * Does something after a Slayable dies.
+     */
+    void death();
+}
