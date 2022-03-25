78827788e8260851d053b0eafe2475709c3d68ff
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-25 10:49:57 -0700

    pls dont push this

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Entity.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Entity.java
@@ -7,20 +7,21 @@ import javafx.scene.image.ImageView;
  * @author FonseLULW
  * @version 1.0
  */
-public abstract class Entity implements Slayable {
+public abstract class Entity implements Slayable, Dynamic {
     /**
      * An Entity's display representation as an ImageView.
      */
     protected final ImageView sprite;
+    protected double speedX;
+    protected double speedY;
 
-    // could maybe be decomposed?
+    // could maybe be decomposed? a class Stats hmm
     protected int maxHP;
     protected int currentHP;
     protected int level;
     protected int damage;
     protected int defence;
     protected double range;
-    protected double speed;
     protected String name;
 
     public Entity(final String filename, final int x, final int y) {
@@ -52,6 +53,28 @@ public abstract class Entity implements Slayable {
         sprite.setY(y);
     }
 
+    @Override
+    public void setSpeedX(final double speed) {
+        speedX = speed;
+    }
+
+    @Override
+    public void setSpeedY(final double speed) {
+        speedY = speed;
+    }
+
+    @Override
+    public void move() {
+        sprite.setX(sprite.getX() + speedX);
+        sprite.setY(sprite.getY() + speedY);
+        System.out.println("" + sprite.getX() + ":" + sprite.getY());
+    }
+
+    @Override
+    public boolean collision() {
+        return false;
+    }
+
     // maybe a new interface??
-    public abstract void attack();
+//    public abstract void attack();
 }
--- src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Game.java
@@ -1,6 +1,7 @@
 package ca.bcit.comp2522.termproject.valhalla.entities;
 
 import javafx.application.Application;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Group;
@@ -12,6 +13,8 @@ public class Game extends Application {
     public static final int APP_HEIGHT = 563;
     private static final String APP_TITLE = "Valhalla";
 
+    Hero hero;
+
     @Override
     public void start(final Stage stage) {
         // application setup
@@ -20,19 +23,20 @@ public class Game extends Application {
         stage.setTitle(APP_TITLE);
 
         // game setup
-        Hero hero = new Hero("baymax.jpg");
+        hero = new Hero("baymax.jpg");
         root.getChildren().add(hero.getSprite());
+        hero.setHeight(50);
+
+        // do we need a clock? while loop?
+        hero.move();
 
         // event listeners
-        scene.setOnKeyPressed(this::handleKeyPress);
+        // this:: ??
+        scene.setOnKeyPressed(this::processKeyPress);
 
         // display the screen
         stage.setScene(scene);
         stage.show();
-    }
-
-    public void handleKeyPress(final KeyEvent event) {
-        System.out.println(event);
     }
 
     /**
