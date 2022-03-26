b5f1339166d4525effbffbcb682c23a5dc332870
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-25 23:59:17 -0700

    Refactored Dynamic to Tangible

---
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/entities/Tangible.java
@@ -0,0 +1,15 @@
+package ca.bcit.comp2522.termproject.valhalla.entities;
+
+import javafx.scene.Node;
+
+/**
+ * A Tangible interface for all that can be touched.
+ * @author FonseLULW
+ * @version 1.0
+ */
+public interface Tangible {
+    Node getSprite();
+    void setWidth(int width);
+    void setHeight(int height);
+    boolean collision(Entity entity);
+}
