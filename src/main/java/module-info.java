9abad685a228fe671c713941e7d7482e1593aac1
kaioh08 <kaka8651@gmail.com>
2022-03-30 13:30:31 -0700

    Committing module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,4 @@
-module ca.bcit.comp2522.termproject.valhalla {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ca.bcit.comp2522.termproject.valhalla to javafx.fxml;
-    exports ca.bcit.comp2522.termproject.valhalla;
-    exports ca.bcit.comp2522.termproject.valhalla.entities;
-    opens ca.bcit.comp2522.termproject.valhalla.entities to javafx.fxml;
-    exports ca.bcit.comp2522.termproject.valhalla.game;
-    opens ca.bcit.comp2522.termproject.valhalla.game to javafx.fxml;
-    exports ca.bcit.comp2522.termproject.valhalla.towers;
-    opens ca.bcit.comp2522.termproject.valhalla.towers to javafx.fxml;
+open module drop.main {
+    requires com.almasb.fxgl.all;
+    requires javafx.media;
 }
