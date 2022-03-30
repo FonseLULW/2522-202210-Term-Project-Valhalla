3ef0a3b9da0329947e23d14b29e448dbb325d3e1
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-29 18:31:46 -0700

    Implemented scene switching functionality in Game

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/Game.java
@@ -2,6 +2,7 @@ package ca.bcit.comp2522.termproject.valhalla.game;
 
 import ca.bcit.comp2522.termproject.valhalla.entities.Hero;
 import javafx.application.Application;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Group;
@@ -14,76 +15,113 @@ import javafx.scene.Scene;
  * @version 1.0
  */
 public class Game extends Application {
+    private Stage gameStage;
+    private Scene currentScene;
+    private Scene gameplayScene;
+    private Scene titleScene;
+
+
     public static final int APP_WIDTH = 1000;
     public static final int APP_HEIGHT = 563;
     private static final String APP_TITLE = "Valhalla";
 
+
     @Override
     public void start(final Stage stage) {
-        // application setup
-        Group root = new Group();
-        Scene scene = new Scene(root, APP_WIDTH, APP_HEIGHT, Color.GAINSBORO);
-        stage.setTitle(APP_TITLE);
+        gameStage = stage;
+        gameStage.setTitle(APP_TITLE);
 
-        // game setup
-        Hero hero = new Hero("![](../../../../../../../../../resources/img/hero1_attack2.png)");
-        root.getChildren().add(hero);
-        root.getChildren();
-        hero.setHeight(60);
+        gameplayScene = initGameplayScene();
+        titleScene = initTitleScene();
+
+        // application setup
+        currentScene = titleScene;
+
+        currentScene.setOnKeyPressed((event) -> {
+            switch (event.getCode()) {
+                case ESCAPE:
+                    switchScene();
+                    break;
+                default:
+                    break;
+            }
+        });
 
         // this is the game loop
         final Clock clock = new Clock() {
             @Override
             public void handle(final long timestamp) {
                 // move
-                hero.move();
+//                hero.move();
             }
         };
         clock.start();
 
-        scene.setOnKeyPressed((event) -> {
-            switch (event.getCode()) {
-                case A:
-                    hero.setVelocityX(-10);
-                    break;
-                case D:
-                    hero.setVelocityX(10);
-                    break;
-                case W:
-                    hero.setVelocityY(-10);
-                    break;
-                case S:
-                    hero.setVelocityY(10);
-                    break;
-                default:
-                    break;
-            }
-
-        });
-
-        scene.setOnKeyReleased((event) -> {
-            switch (event.getCode()) {
-                case A:
-                    hero.setVelocityX(0);
-                    break;
-                case D:
-                    hero.setVelocityX(0);
-                    break;
-                case W:
-                    hero.setVelocityY(0);
-                    break;
-                case S:
-                    hero.setVelocityY(0);
-                    break;
-                default:
-                    break;
-            }
-
-        });
+//        currentScene.setOnKeyPressed((event) -> {
+//            switch (event.getCode()) {
+//                case A:
+//                    hero.setVelocityX(-10);
+//                    break;
+//                case D:
+//                    hero.setVelocityX(10);
+//                    break;
+//                case W:
+//                    hero.setVelocityY(-10);
+//                    break;
+//                case S:
+//                    hero.setVelocityY(10);
+//                    break;
+//                default:
+//                    break;
+//            }
+//
+//        });
+//
+//        currentScene.setOnKeyReleased((event) -> {
+//            switch (event.getCode()) {
+//                case A:
+//                    hero.setVelocityX(0);
+//                    break;
+//                case D:
+//                    hero.setVelocityX(0);
+//                    break;
+//                case W:
+//                    hero.setVelocityY(0);
+//                    break;
+//                case S:
+//                    hero.setVelocityY(0);
+//                    break;
+//                default:
+//                    break;
+//            }
+//
+//        });
 
         // display the screen
-        stage.setScene(scene);
+        stage.setScene(currentScene);
         stage.show();
+    }
+
+    public Scene initGameplayScene() {
+        Group root = new Group();
+        gameplayScene = new Scene(root, APP_WIDTH, APP_HEIGHT, Color.GAINSBORO);
+
+        // stuff inside gameplay scene
+        Hero hero = new Hero("hero1_idle.jpeg");
+        root.getChildren().add(hero);
+        hero.setHeight(60);
+        return gameplayScene;
+    }
+
+    public Scene initTitleScene() {
+        Group root = new Group();
+        titleScene = new Scene(root, APP_WIDTH, APP_HEIGHT, Color.BURLYWOOD);
+        return titleScene;
+    }
+
+    public void switchScene() {
+        currentScene = gameplayScene;
+        gameStage.setScene(currentScene);
     }
 
     /**
--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/SceneManager.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/SceneManager.java
@@ -1,4 +1,9 @@
 package ca.bcit.comp2522.termproject.valhalla.game;
 
+import javafx.scene.Scene;
+
 public class SceneManager {
+
+    private Scene loginScene;
+    private Scene gameplayScene;
 }
