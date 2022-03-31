75d4875c591c4e243ba92c5cc0e32c45aad5c8d3
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-31 09:55:58 -0700

    Added buttons on main menu

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaMenu.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaMenu.java
@@ -4,6 +4,8 @@ import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.beans.binding.StringBinding;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
@@ -17,25 +19,38 @@ public class ValhallaMenu extends FXGLMenu {
         ValhallaButton btnPlay = new ValhallaButton("Play", () -> {
             System.out.println("playing game lol");
         });
+        ValhallaButton btnExit = new ValhallaButton("Exit", () -> {
+            System.out.println("exiting game lol");
+        });
+        ValhallaButton btnSettings = new ValhallaButton("Settings", () -> {
+            System.out.println("editing game lol");
+        });
+        btnPlay.setTranslateX(100);
+        btnSettings.setTranslateX(200);
+        btnExit.setTranslateX(300);
 
         addChild(createBackground(Game.APP_WIDTH, Game.APP_HEIGHT));
         addChild(btnPlay);
+        addChild(btnExit);
+        addChild(btnSettings);
     }
 
-    protected Node createBackground(double w, double h) {
+    protected Node createBackground(final double width, final double height) {
         return FXGL.texture("menu.png", Game.APP_WIDTH, Game.APP_HEIGHT);
     }
 
-    private class ValhallaButton extends Button {
-        private String name;
-        private Runnable runnable;
+    private static class ValhallaButton extends Button {
+        private static final double BTN_WIDTH = 100;
+        private final String name;
+        private final Runnable runnable;
 
         public ValhallaButton(final String name, final Runnable runnable) {
             this.name = name;
             this.runnable = runnable;
 
-            setMinWidth(100);
+            setMinWidth(BTN_WIDTH);
             setText(name);
+            setOnAction(actionEvent -> runnable.run());
         }
     }
 }
