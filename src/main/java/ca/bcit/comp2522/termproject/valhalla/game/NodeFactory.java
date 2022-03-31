cf2e890f4d23e730ae74d822131da11d79b971fc
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-30 22:28:33 -0700

    Started integration of FXGL, moved old code to a backup

---
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/NodeFactory.java
@@ -0,0 +1,17 @@
+package ca.bcit.comp2522.termproject.valhalla.game;
+
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.Spawns;
+
+/**
+ * A NodeFactory class made to make new game entities called nodes and add them to the game environment.
+ * @author FonseLULW
+ * @author kaioh08
+ * @version 1.0
+ */
+public class NodeFactory implements EntityFactory {
+
+//    @Spawns("hero")
+//    public Entity
+}
