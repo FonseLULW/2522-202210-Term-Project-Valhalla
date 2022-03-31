f3a7e948626b801cc14b73b2e8626cc15c227e79
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-30 22:55:34 -0700

    Implemented game node spawning

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/Game.java
@@ -2,10 +2,19 @@ package ca.bcit.comp2522.termproject.valhalla.game;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
 
 public class Game extends GameApplication {
     @Override
     protected void initSettings(final GameSettings gameSettings) {
+
+    }
+
+    @Override
+    protected void initGame() {
+        FXGL.getGameWorld().addEntityFactory(new NodeFactory());
+
+        FXGL.spawn("hero", 60, 60);
 
     }
 
--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/NodeFactory.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/NodeFactory.java
@@ -1,8 +1,12 @@
 package ca.bcit.comp2522.termproject.valhalla.game;
 
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  * A NodeFactory class made to make new game entities called nodes and add them to the game environment.
@@ -12,6 +16,10 @@ import com.almasb.fxgl.entity.Spawns;
  */
 public class NodeFactory implements EntityFactory {
 
-//    @Spawns("hero")
-//    public Entity
+    @Spawns("hero")
+    public Entity newHero(final SpawnData data) {
+        return FXGL.entityBuilder(data)
+                .view("hero1_idle.png")
+                .build();
+    }
 }
