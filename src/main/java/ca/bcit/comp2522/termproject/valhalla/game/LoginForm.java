deaaccbd08a4df50527a2e1aa6739477aaeae048
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-31 12:44:06 -0700

    Resized LoginForm

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/LoginForm.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/LoginForm.java
@@ -6,14 +6,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
 public class LoginForm extends AnchorPane {
-    public static final double WIDTH = 500;
-    public static final double HEIGHT = 500;
-
-    public static final double TOP_PADDING = 5.0;
+    public static final double TOP_PADDING = 10.0;
     public static final double BOTTOM_PADDING = 5.0;
-    public static final double LEFT_PADDING = 5.0;
-    public static final double RIGHT_PADDING = 5.0;
-    public static final double FIELD_HEIGHT = 20.0;
+    public static final double LEFT_PADDING = 10.0;
+    public static final double RIGHT_PADDING = 10.0;
+    public static final double ROW_HEIGHT = 20.0;
+
+    public static final double WIDTH = 300 + LEFT_PADDING + RIGHT_PADDING;
+    public static final double HEIGHT = 100 + TOP_PADDING + BOTTOM_PADDING;
 
     public LoginForm() {
         super();
@@ -27,14 +27,14 @@ public class LoginForm extends AnchorPane {
         setLeftAnchor(username, LEFT_PADDING);
         setTopAnchor(username, TOP_PADDING);
         setLeftAnchor(password, LEFT_PADDING);
-        setTopAnchor(password, TOP_PADDING * 2 + FIELD_HEIGHT + BOTTOM_PADDING);
+        setTopAnchor(password, TOP_PADDING * 2 + ROW_HEIGHT + BOTTOM_PADDING);
         getChildren().addAll(username, password);
 
     }
 
     public static class FormRow extends HBox {
         private static final int LABEL_RELATIVE_WIDTH = 4;
-        private static final int FIELD_RELATIVE_WIDTH = 3;
+        private static final int FIELD_RELATIVE_WIDTH = 2;
 
         private final Label label;
         private TextField field;
@@ -46,8 +46,8 @@ public class LoginForm extends AnchorPane {
             setStyle("-fx-background-color: #af4c4c");
             this.label = new Label(label);
             this.field = new TextField(placeholder);
-            this.field.setPrefHeight(FIELD_HEIGHT);
-            this.label.setMinHeight(FIELD_HEIGHT);
+            this.field.setPrefHeight(ROW_HEIGHT);
+            this.label.setMinHeight(ROW_HEIGHT);
             this.label.setLabelFor(this.field);
             this.field.setPrefWidth(width / FIELD_RELATIVE_WIDTH);
             this.label.setPrefWidth(width / LABEL_RELATIVE_WIDTH);
