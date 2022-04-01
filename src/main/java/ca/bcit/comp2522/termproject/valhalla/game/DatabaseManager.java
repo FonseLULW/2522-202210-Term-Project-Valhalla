21cd943028cc50d54a8c7748f8f85ff93fe2b3f0
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-31 20:49:10 -0700

    Accessed sample database using DatabaseManager

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/DatabaseManager.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/DatabaseManager.java
@@ -8,47 +8,51 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DatabaseManager {
-    private DatabaseManager() { }
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final int PORT = 3306;
+    private static final String URL = "jdbc:mysql://localhost:" + PORT + "/";
 
-    /**
-     * Drives the example.
-     *
-     * @param args unused
-     * @throws SQLException if a connection cannot be made.
-     * @throws ClassNotFoundException if the MySQL ConnectorJ JDBC JAR cannot be found, i.e., is not on classpath
-     */
-    public static void main(final String[] args) throws SQLException, ClassNotFoundException {
+    private final Connection connection;
 
-        // We register the Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public DatabaseManager(final String database, final String username, final String password)
+            throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER);
 
-        // We identify the driver, the rdbms, the host, the port, and the schema name
-        final String url = "jdbc:mysql://localhost:3306/comp2522";
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", username);
+        connectionProperties.put("password", password);
 
-        // We need to send a user and a password when we try to connect!
-        final Properties connectionProperties = new Properties();
-        connectionProperties.put("user", "root");
-        connectionProperties.put("password", "eggonomics");
+        connection = DriverManager.getConnection(URL + database, connectionProperties);
+    }
 
-        // We establish a connection...
-        final Connection connection = DriverManager.getConnection(url, connectionProperties);
-        if (connection != null) {
-            System.out.println("Successfully connected to MySQL database test");
-        }
-
-        // Create a statement to send on the connection...
-        assert connection != null;
+    private ResultSet query(final String query) throws SQLException {
         Statement stmt = connection.createStatement();
+        return stmt.executeQuery(query);
+    }
 
-        // Execute the statement and receive the result...
-        ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+//    public ResultSet search(final String table, final String username) {
+//        try {
+//            return query("SELECT * FROM " + table);
+//        } catch (SQLException e) {
+//            return null;
+//        }
+//    }
 
-        // And then display the result!
-        System.out.println("user_id\t\tpassword");
-        while (rs.next()) {
-            String userID = rs.getString("user_id");
-            String password = rs.getString("password");
-            System.out.println(userID + "\t\t" + password);
+    public static void main(final String[] args) {
+        DatabaseManager db;
+        ResultSet rs;
+        try {
+            db = new DatabaseManager("comp2522", "root", "eggonomics");
+            rs = db.query("SELECT * FROM USERS");
+
+            System.out.println("user_id\t\tpassword");
+            while (rs.next()) {
+                String userID = rs.getString("user_id");
+                String password = rs.getString("password");
+                System.out.println(userID + "\t\t" + password);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,5 @@ open module drop.main {
     requires com.almasb.fxgl.all;
     requires javafx.media;
     requires java.sql;
+    requires mysql.connector.java;
 }
