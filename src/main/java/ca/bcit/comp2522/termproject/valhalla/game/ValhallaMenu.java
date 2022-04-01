8bfb0671557758132d69ffdbb24b3ce5139f3b9c
Fonse Clarito <jalfonsclarito@gmail.com>
2022-03-31 20:13:32 -0700

    Laid out menu elements

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaMenu.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaMenu.java
@@ -4,21 +4,28 @@ import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.scene.Node;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class ValhallaMenu extends FXGLMenu {
+    private static final double MENU_X = Game.APP_WIDTH / 2.0 - ValhallaButton.BTN_WIDTH / 2;
+    private static final double MENU_Y = Game.APP_HEIGHT / 2.0;
 
     public ValhallaMenu() {
         super(MenuType.MAIN_MENU);
         addChild(createBackground());
-
         // create menu elements here; menu elements must extend javafx Node
-        ValhallaButton btnExit = new ValhallaButton("Exit", 100, 200);
-        ValhallaButton btnPlay = new ValhallaButton("Play", 100, 100);
+        ValhallaButton btnPlay = new ValhallaButton("Play", 0, 0);
+        ValhallaButton btnExit = new ValhallaButton("Exit", 0, 25);
+        VBox menu = createMenu();
+        menu.getChildren().addAll(btnPlay, btnExit);
 
         // create login form
+        final double offset = 150;
         LoginForm loginForm = new LoginForm();
-        loginForm.setTranslateX(50);
-        loginForm.setTranslateY(50);
+        loginForm.setTranslateX(MENU_X + offset);
+        loginForm.setTranslateY(MENU_Y);
         loginForm.addField(LoginForm.Fields.USERNAME, "Username: ");
         loginForm.addField(LoginForm.Fields.PASSWORD, "Password: ");
         loginForm.setVisible(false);
@@ -27,19 +34,42 @@ public class ValhallaMenu extends FXGLMenu {
         btnPlay.setAction(() -> {
             loginForm.setVisible(true);
         });
-        btnExit.setAction(this::fireExit);
-        loginForm.getSubmit().setAction(this::fireNewGame);
         loginForm.getCancel().setAction(() -> {
             loginForm.setVisible(false);
         });
+        btnExit.setAction(this::fireExit);
+        loginForm.getSubmit().setAction(() -> {
+            if (validate()) {
+                loadGame();
+            }
+        });
 
         // add menu elements to display here
-        addChild(btnPlay);
-        addChild(btnExit);
+        addChild(menu);
         addChild(loginForm);
     }
 
-    protected Node createBackground() {
+    private Node createBackground() {
         return FXGL.texture("menu.png", Game.APP_WIDTH, Game.APP_HEIGHT);
+    }
+
+    private VBox createMenu() {
+        VBox menu = new VBox();
+        final double spaceBetweenElements = 20;
+        menu.setSpacing(spaceBetweenElements);
+        menu.setTranslateX(MENU_X);
+        menu.setTranslateY(MENU_Y);
+        menu.setStyle("-fx-background-color: #af4c4c");
+        return menu;
+    }
+
+    private boolean validate() {
+        return true;
+    }
+
+    private void loadGame() {
+        // if new player fireNewGame
+        // else get load file of user and fireLoad
+        fireNewGame();
     }
 }
