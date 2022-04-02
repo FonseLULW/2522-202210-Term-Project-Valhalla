d523f06b039d82071746896e26cb80e86778ba82
Fonse Clarito <jalfonsclarito@gmail.com>
2022-04-02 00:25:13 -0700

    Fixed checkstyle complaints, add docs

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaMenu.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaMenu.java
@@ -4,20 +4,28 @@ import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.scene.Node;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
+/**
+ * A ValhallaMenu class representing the game's main menu.
+ * @author FonseLULW
+ * @author kaioh08
+ * @version 1.0
+ */
 public class ValhallaMenu extends FXGLMenu {
     private static final double MENU_X = Game.APP_WIDTH / 2.0 - ValhallaButton.BTN_WIDTH / 2;
     private static final double MENU_Y = Game.APP_HEIGHT / 2.0;
 
+    /**
+     * Constructs a new ValhallaMenu object.
+     */
     public ValhallaMenu() {
         super(MenuType.MAIN_MENU);
         addChild(createBackground());
         // create menu elements here; menu elements must extend javafx Node
+        final double spaceBetweenBtns = 25;
         ValhallaButton btnPlay = new ValhallaButton("Play", 0, 0);
-        ValhallaButton btnExit = new ValhallaButton("Exit", 0, 25);
+        ValhallaButton btnExit = new ValhallaButton("Exit", 0, spaceBetweenBtns);
         VBox menu = createMenu();
         menu.getChildren().addAll(btnPlay, btnExit);
 
