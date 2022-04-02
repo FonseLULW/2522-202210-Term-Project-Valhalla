399ccab06d4c1bb939dfbdbd8c0f2eff4d7c7800
Fonse Clarito <jalfonsclarito@gmail.com>
2022-04-02 00:21:09 -0700

    Fixed checkstyle complaints, add docs

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaButton.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/ValhallaButton.java
@@ -1,23 +1,33 @@
 package ca.bcit.comp2522.termproject.valhalla.game;
 
-import com.almasb.fxgl.texture.Texture;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
 
-import java.sql.Array;
-
+/**
+ * A ValhallaButton class representing buttons custom to Valhalla.
+ * @author FonseLULW
+ * @author kaioh08
+ * @version 1.0
+ */
 public class ValhallaButton extends Button {
+    /**
+     * The button width.
+     */
     public static final double BTN_WIDTH = 100;
+    /**
+     * The button height.
+     */
     public static final double BTN_HEIGHT = 20;
-    private final String label;
     private Runnable action;
 
+    /**
+     * Constructs a new ValhallaButton object.
+     * @param label a String displayed on this ValhallaButton
+     * @param x a double representing the x-coordinates of this ValhallaButton
+     * @param y a double representing the y-coordinates of this ValhallaButton
+     * @param action a Runnable that will be executed when this ValhallaButton is pressed
+     */
     public ValhallaButton(final String label, final double x, final double y, final Runnable action) {
         super(label);
-        this.label = label;
         this.action = action;
 
         setMinWidth(BTN_WIDTH);
@@ -27,10 +37,20 @@ public class ValhallaButton extends Button {
         setTranslateY(y);
     }
 
+    /**
+     * Constructs a new ValhallaButton object.
+     * @param label a String displayed on this ValhallaButton
+     * @param x a double representing the x-coordinates of this ValhallaButton
+     * @param y a double representing the y-coordinates of this ValhallaButton
+     */
     public ValhallaButton(final String label, final double x, final double y) {
         this(label, x, y, null);
     }
 
+    /**
+     * Sets the action of this ValhallaButton.
+     * @param action a Runnable that will be executed when this ValhallaButton is pressed
+     */
     public void setAction(final Runnable action) {
         this.action = action;
     }
