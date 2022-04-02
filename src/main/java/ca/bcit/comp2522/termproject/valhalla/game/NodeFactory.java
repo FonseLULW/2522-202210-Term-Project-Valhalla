d1877e94896851fd7fd12139fbce299b4c2aced8
Fonse Clarito <jalfonsclarito@gmail.com>
2022-04-01 23:24:25 -0700

    Made minor scale changes for Hero character

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/NodeFactory.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/NodeFactory.java
@@ -1,12 +1,17 @@
 package ca.bcit.comp2522.termproject.valhalla.game;
 
+import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.dsl.components.ProjectileComponent;
+import com.almasb.fxgl.dsl.components.*;
+import com.almasb.fxgl.dsl.components.view.TrailParticleComponent;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.texture.Texture;
 import javafx.geometry.Point2D;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -17,12 +22,12 @@ import javafx.scene.shape.Rectangle;
  * @version 1.0
  */
 public class NodeFactory implements EntityFactory {
-
     @Spawns("hero")
     public Entity newHero(final SpawnData data) {
         return FXGL.entityBuilder(data)
                 .view("hero1_idle.png")
                 .with(new ProjectileComponent(new Point2D(1, 0), 150))
+                .scale(0.1, 0.1)
                 .build();
     }
 }
