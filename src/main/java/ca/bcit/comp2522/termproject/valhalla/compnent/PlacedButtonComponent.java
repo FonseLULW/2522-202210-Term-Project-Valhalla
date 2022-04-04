37be11ff8d5cfa45d2f2e4e37b1356c11f41b6bd
kaioh08 <kaka8651@gmail.com>
2022-04-04 01:50:36 -0700

    Committing PlacedButtonComponent class for game

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/compnent/PlacedButtonComponent.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/compnent/PlacedButtonComponent.java
@@ -8,11 +8,18 @@ import ca.bcit.comp2522.termproject.valhalla.constant.TowerType;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 
+/**
+ * A placedButtonComponent class that extends from Component.
+ *
+ * @author kaioh
+ * @version 1.0
+ */
 public class PlacedButtonComponent extends Component {
-    private static final AnimationChannel AC_BORDER = new AnimationChannel(FXGL.image("selected_border.png"), 5, 80, 80, Duration.seconds(1), 0, 14);
+    private static final AnimationChannel BORDER_ANIMATION = new AnimationChannel(FXGL.image("selected_border.png"),
+            5, 80, 80, Duration.seconds(1), 0, 14);
     private AnimatedTexture texture;
     private final TowerType towerType;
-    private boolean selected;
+    private boolean isSelected;
 
     public PlacedButtonComponent(final TowerType towerType) {
         this.towerType = towerType;
@@ -20,17 +27,28 @@ public class PlacedButtonComponent extends Component {
 
     @Override
     public void onAdded() {
-        texture = new AnimatedTexture(AC_BORDER);
+        texture = new AnimatedTexture(BORDER_ANIMATION);
         texture.setVisible(false);
         entity.getViewComponent().addChild(texture);
 
         entity.getViewComponent().addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
-            selected = !selected;
-            if (selected) {
+            isSelected = !isSelected;
+            if (isSelected) {
                 FXGL.set("towerType", towerType);
             } else {
                 FXGL.set("towerType", TowerType.NONE);
             }
         });
+    }
+
+    public void setSelected(final boolean selected) {
+        this.isSelected = selected;
+        if (selected) {
+            texture.setVisible(true);
+            texture.loop();
+        } else {
+            texture.stop();
+            texture.setVisible(false);
+        }
     }
 }
