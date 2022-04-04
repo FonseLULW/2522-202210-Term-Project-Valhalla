4b4b33e9047607629550ed9cde7380eaf7408149
kaioh08 <kaka8651@gmail.com>
2022-04-04 03:47:40 -0700

    Fixing Config interface

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/constant/Config.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/constant/Config.java
@@ -9,7 +9,7 @@ public interface Config {
     int LOWER_BOUND = 10;
     int UPPER_BOUND = 15;
 
-    TowerData ARROW_TOWER_DATA = new TowerData("arrowTower", 43, 68, 1, 580, 467, Duration.seconds(0.7), FXGL.image("tower/arrow/tower_icon.png")) {
+    TowerData ARROW_TOWER_DATA = new TowerData("arrowTower", 43, 68, 1, 580, 467, Duration.seconds(0.7), FXGL.image("tower_image.png")) {
         @Override
         public int getDamage() {
             return FXGLMath.random(LOWER_BOUND, UPPER_BOUND);
