b395f5d17ba4d44baeb04183c5f584768c40ad4a
kaioh08 <kaka8651@gmail.com>
2022-04-04 01:13:03 -0700

    Committing BulletComponent class for tower

---
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/compnent/BulletComponent.java
@@ -0,0 +1,38 @@
+package ca.bcit.comp2522.termproject.valhalla.compnent;
+
+import com.almasb.fxgl.entity.component.Component;
+import javafx.geometry.Point2D;
+
+public class BulletComponent extends Component {
+
+    private Point2D initPosition;
+
+    private final int radius;
+    private final int damage;
+
+    public BulletComponent(final int radius, final int damage) {
+        this.radius = radius;
+        this.damage = damage;
+    }
+
+    @Override
+    public void onAdded() {
+        initPosition = entity.getPosition();
+    }
+
+    @Override
+    public void onUpdate(final double tpf) {
+        Point2D position = entity.getPosition();
+        if (position.distance(initPosition) > radius) {
+            if (entity.isActive()) {
+                entity.removeFromWorld();
+            }
+        }
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+}
+
