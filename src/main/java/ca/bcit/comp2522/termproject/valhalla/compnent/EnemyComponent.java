49e180339d25847738a8293f81e184d35688f577
kaioh08 <kaka8651@gmail.com>
2022-04-05 12:00:16 -0700

    Fixing EnemyComponent class to display the enemy

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/compnent/EnemyComponent.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/compnent/EnemyComponent.java
@@ -1,5 +1,6 @@
 package ca.bcit.comp2522.termproject.valhalla.compnent;
 
+import ca.bcit.comp2522.termproject.valhalla.game.Game;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.dsl.components.HealthIntComponent;
 import com.almasb.fxgl.entity.component.Component;
@@ -21,19 +22,16 @@ public class EnemyComponent extends Component {
     int index;
     private final ProgressBar hpBar;
     private final AnimatedTexture texture;
-    private final AnimationChannel animDie;
+    private final AnimationChannel walkingRight = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
+    private final AnimationChannel walkingLeft = new AnimationChannel(FXGL.image("enemy/slugman_2.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
+    private final AnimationChannel walkingUp = new AnimationChannel(FXGL.image("enemy/slugman_3.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
+    private final AnimationChannel walkingDown = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
+
     private boolean dead;
 
     public EnemyComponent(final ProgressBar hpBar) {
         this.hpBar = hpBar;
-        AnimationChannel walkingRight = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
-        AnimationChannel walkingLeft = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
-        AnimationChannel walkingUp = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
-        AnimationChannel walkingDown = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 5 * 48, 48 * 3), 5, 48, 48, Duration.seconds(.5), 0, 14);
-        animDie = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 5 * 48, 2 * 48), 5, 48, 48, Duration.seconds(.25), 0, 8);
-
         texture = new AnimatedTexture(walkingRight);
-
     }
 
     public boolean isDead() {
@@ -49,9 +47,53 @@ public class EnemyComponent extends Component {
         if (hp.isZero()) {
             dead = true;
             entity.getViewComponent().removeChild(hpBar);
-            texture.loopAnimationChannel(animDie);
             texture.setOnCycleFinished(() -> entity.removeFromWorld());
         }
 
+    }
+
+    @Override
+    public void onAdded() {
+        entity.getViewComponent().addChild(texture);
+        Game app = (Game) (FXGL.getApp());
+        pointInfos = app.getPointInfos();
+        nextWaypoint = pointInfos.get(index).getKey();
+        walkingAnimation();
+    }
+
+    private void walkingAnimation() {
+        String dir = pointInfos.get(index).getValue();
+        if ("right".equals(dir)) {
+            texture.loopAnimationChannel(walkingRight);
+        } else if ("left".equals(dir)) {
+            texture.loopAnimationChannel(walkingLeft);
+        } else if ("up".equals(dir)) {
+            texture.loopAnimationChannel(walkingUp);
+        } else if ("down".equals(dir)) {
+            texture.loopAnimationChannel(walkingDown);
+        }
+    }
+
+    @Override
+    public void onUpdate(final double tpf) {
+        if (index >= pointInfos.size() || dead) {
+            return;
+        }
+        speed = tpf * 30 * 2;
+
+        Point2D velocity = nextWaypoint.subtract(entity.getPosition())
+                .normalize()
+                .multiply(speed);
+
+        entity.translate(velocity);
+
+        if (nextWaypoint.distance(entity.getPosition()) < speed) {
+            entity.setPosition(nextWaypoint);
+            walkingAnimation();
+            index++;
+            if (index < pointInfos.size()) {
+                nextWaypoint = pointInfos.get(index).getKey();
+            }
+        }
     }
 }
