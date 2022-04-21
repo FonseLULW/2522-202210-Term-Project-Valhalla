4d88fbc44181a4f3d16841d7640baf39ebe6ca31
kaioh08 <kaka8651@gmail.com>
2022-04-21 15:57:54 -0700

    Added Hejo to enum

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/constant/GameType.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/constant/GameType.java
@@ -4,10 +4,12 @@ package ca.bcit.comp2522.termproject.valhalla.constant;
  * An Enum class of game variables.
  *
  * @author kaioh
+ * @author FonseLULW
  * @version 1.0
  */
 public enum GameType {
     HERO,
+    HEJO,
     ENEMY,
     TOWER,
     EMPTY,
