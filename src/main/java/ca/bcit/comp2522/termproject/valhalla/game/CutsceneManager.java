2e0ff7303f27d677e6060ba5479131b2fa471f5d
Fonse Clarito <jalfonsclarito@gmail.com>
2022-04-22 13:56:46 -0700

    Refactored class into a record

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/game/CutsceneManager.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/game/CutsceneManager.java
@@ -2,21 +2,34 @@ package ca.bcit.comp2522.termproject.valhalla.game;
 
 import com.almasb.fxgl.cutscene.Cutscene;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.texture.Texture;
 import javafx.util.Duration;
-
-import java.util.List;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getAssetLoader;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.runOnce;
 
-public class CutsceneManager {
-    private final String cutsceneBackgroundFilename;
-
-    public CutsceneManager(final String cutsceneBackgroundFilename) {
-        this.cutsceneBackgroundFilename = cutsceneBackgroundFilename;
+/**
+ * A CutsceneManager class.
+ * @author FonseLULW
+ * @author kaioh
+ * @version 1.0
+ * @param cutsceneBackgroundFilename a String representing the filename of the background displayed during the cutscene.
+ */
+public record CutsceneManager(String cutsceneBackgroundFilename) {
+    /**
+     * Constructs a new CutsceneManager.
+     *
+     * @param cutsceneBackgroundFilename a String representing the filename of the cutscene background
+     */
+    public CutsceneManager {
     }
 
+    /**
+     * Plays the cutscene.
+     *
+     * @param cutsceneFilename a String representing a txt file containing the cutscene script formatted
+     *                         according to FXGL's cutscene format
+     * @param afterCutscene    a Runnable that runs after the cutscene ends
+     */
     public void playCutscene(final String cutsceneFilename, final Runnable afterCutscene) {
         runOnce(() -> {
             var cutsceneBackground = FXGL.texture(cutsceneBackgroundFilename, Game.APP_WIDTH, Game.APP_HEIGHT);
@@ -33,6 +46,12 @@ public class CutsceneManager {
         }, Duration.seconds(1));
     }
 
+    /**
+     * Plays the cutscene.
+     *
+     * @param cutsceneFilename a String representing a txt file containing the cutscene script formatted
+     *                         according to FXGL's cutscene format
+     */
     public void playCutscene(final String cutsceneFilename) {
         playCutscene(cutsceneFilename, null);
     }
