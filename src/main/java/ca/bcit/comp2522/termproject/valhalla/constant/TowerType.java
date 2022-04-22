85e666e7959a5cafad9f7c0f6b5c76405c71e1e2
kaioh08 <kaka8651@gmail.com>
2022-04-22 02:34:56 -0700

    documenting TowerType

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/constant/TowerType.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/constant/TowerType.java
@@ -4,6 +4,7 @@ package ca.bcit.comp2522.termproject.valhalla.constant;
  * A enum class of Tower types in this game.
  *
  * @author kaioh
+ * @author FonseLULW
  * @version 1.0
  */
 public enum TowerType {
