19f0c4c5449a271a732fc61a90fda71ff7ee1779
kaioh08 <kaka8651@gmail.com>
2022-04-22 02:11:33 -0700

    Properly documenting BulletComponent

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/component/BulletComponent.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/component/BulletComponent.java
@@ -3,6 +3,13 @@ package ca.bcit.comp2522.termproject.valhalla.component;
 import com.almasb.fxgl.entity.component.Component;
 import javafx.geometry.Point2D;
 
+/**
+ * A BulletComponent class.
+ *
+ * @author kaioh
+ * @author FonseLULW
+ * @version 1.0
+ */
 public class BulletComponent extends Component {
 
     private Point2D initPosition;
@@ -10,16 +17,28 @@ public class BulletComponent extends Component {
     private final int radius;
     private final int damage;
 
+    /**
+     * A bullet component.
+     * @param radius an int value of the radius
+     * @param damage an int value of damage
+     */
     public BulletComponent(final int radius, final int damage) {
         this.radius = radius;
         this.damage = damage;
     }
 
+    /**
+     * An onAdded function to get the position of the bullet.
+     */
     @Override
     public void onAdded() {
         initPosition = entity.getPosition();
     }
 
+    /**
+     * An override function that will update bullet.
+     * @param tpf a double
+     */
     @Override
     public void onUpdate(final double tpf) {
         Point2D position = entity.getPosition();
@@ -30,6 +49,10 @@ public class BulletComponent extends Component {
         }
     }
 
+    /**
+     * A getDamage function that will get damage.
+     * @return damage
+     */
     public int getDamage() {
         return damage;
     }
