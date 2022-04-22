9579bdb5f0db1124da71c30aa70fcacd413014f2
kaioh08 <kaka8651@gmail.com>
2022-04-22 02:26:38 -0700

    documenting EnemyComponent

--- src/main/java/ca/bcit/comp2522/termproject/valhalla/component/EnemyComponent.java
+++ src/main/java/ca/bcit/comp2522/termproject/valhalla/component/EnemyComponent.java
@@ -1,6 +1,5 @@
 package ca.bcit.comp2522.termproject.valhalla.component;
 
-import ca.bcit.comp2522.termproject.valhalla.constant.GameType;
 import ca.bcit.comp2522.termproject.valhalla.game.Game;
 import com.almasb.fxgl.core.collection.PropertyMap;
 import com.almasb.fxgl.dsl.FXGL;
@@ -19,43 +18,66 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * An EnemyComponent class.
+ *
+ * @author kaioh
+ * @author FonseLULW
+ * @version 1.0
+ */
 public class EnemyComponent extends Component {
 
-    private HealthIntComponent hp;
+    /**
+     * Declaring the value of the index.
+     */
+    int index;
+    private final HealthIntComponent hp;
     private LinkedHashMap<Integer, Pair<Point2D, String>> pointInfos;
     private Point2D nextWaypoint;
-    private double speed;
-    int index;
     private final ProgressBar hpBar;
     private final AnimatedTexture texture;
-    private final AnimationChannel walkingRight
-    = new AnimationChannel(List.of(
-                new Image("assets/textures/enemy/slugman_1.png", 48, 48, true, true),
-                new Image("assets/textures/enemy/slugman_2.png", 48, 48, true, true),
-                new Image("assets/textures/enemy/slugman_3.png", 48, 48, true, true),
-                new Image("assets/textures/enemy/slugman_2.png", 48, 48, true, true)
-        ), Duration.seconds(0.3));
-//            = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 48, 48), 5, 48, 48, Duration.seconds(1), 0, 14);
-//    private final AnimationChannel walkingLeft = new AnimationChannel(FXGL.image("enemy/slugman_2.png", 48, 48), 5, 48, 48, Duration.seconds(1), 0, 14);
-//    private final AnimationChannel walkingUp = new AnimationChannel(FXGL.image("enemy/slugman_3.png", 48, 48), 5, 48, 48, Duration.seconds(1), 0, 14);
-//    private final AnimationChannel walkingDown = new AnimationChannel(FXGL.image("enemy/slugman_1.png", 48, 48), 5, 48, 48, Duration.seconds(1), 0, 14);
 
     private boolean dead;
 
-    public EnemyComponent(final ProgressBar hpBar) {
+    /**
+     * Spawns the new enemy.
+     * @param hp a healthIntComponent
+     * @param hpBar a progressbar
+     */
+    public EnemyComponent(final HealthIntComponent hp, final ProgressBar hpBar) {
+        this.hp = hp;
         this.hpBar = hpBar;
+        final int fortyEight = 48;
+        final double durationTime = 0.3;
+        AnimationChannel walkingRight = new AnimationChannel(List.of(
+                new Image("assets/textures/enemy/slugman_1.png", fortyEight, fortyEight, true, true),
+                new Image("assets/textures/enemy/slugman_2.png", fortyEight, fortyEight, true, true),
+                new Image("assets/textures/enemy/slugman_3.png", fortyEight, fortyEight, true, true),
+                new Image("assets/textures/enemy/slugman_2.png", fortyEight, fortyEight, true, true)
+        ), Duration.seconds(durationTime));
         texture = new AnimatedTexture(walkingRight);
-//        texture.setInterpolator(Interpolator.EASE_BOTH);
     }
 
+    /**
+     * Checks if the enemy is dead.
+     * @return boolean value
+     */
     public boolean isDead() {
         return dead;
     }
 
+    /**
+     * Sets the entity status to dead.
+     * @param dead a boolean value
+     */
     public void setDead(final boolean dead) {
         this.dead = dead;
     }
 
+    /**
+     * Updates the hp bar of the enemy.
+     * @param damage an int value of a damage
+     */
     public void attacked(final int damage) {
         hp.damage(damage);
         if (hp.isZero()) {
@@ -63,39 +85,39 @@ public class EnemyComponent extends Component {
             FXGL.play("slugmandefeat.wav");
             entity.getViewComponent().removeChild(hpBar);
             entity.removeFromWorld();
-//            texture.setOnCycleFinished(() -> entity.removeFromWorld());
         }
 
     }
 
+    /**
+     * Adds enemy to the game.
+     */
     @Override
     public void onAdded() {
+        final double originSetter = 48.0;
         entity.getViewComponent().addChild(texture);
-        entity.setScaleOrigin(new Point2D(48.0 / 2, 48.0 / 2));
+        entity.setScaleOrigin(new Point2D(originSetter / 2, originSetter / 2));
 
         Game app = (Game) (FXGL.getApp());
         pointInfos = app.getPointInfos();
         nextWaypoint = pointInfos.get(index).getKey();
 
         texture.setInterpolator(Interpolator.EASE_IN);
-//        walkingAnimation();
     }
 
     private void walkingAnimation() {
-//        String dir = pointInfos.get(index).getValue();
-//        if ("right".equals(dir)) {
             texture.loop();
-//        } else if ("left".equals(dir)) {
-//            texture.loopAnimationChannel(walkingLeft);
-//        } else if ("up".equals(dir)) {
-//            texture.loopAnimationChannel(walkingUp);
-//        } else if ("down".equals(dir)) {
-//            texture.loopAnimationChannel(walkingDown);
-//        }
     }
 
+    /**
+     * Updates the damage to the enemy.
+     * @param tpf a double
+     */
     @Override
     public void onUpdate(final double tpf) {
+        final int thirty = 30;
+        final int lowerBoundNumber = 15;
+        final int upperBoundNumber = 50;
         if (index >= pointInfos.size() || dead) {
             return;
         }
@@ -105,15 +127,11 @@ public class EnemyComponent extends Component {
         } else {
             texture.setScaleX(1);
         }
-
-        speed = tpf * 30 * 2;
-
+        double speed = tpf * thirty * 2;
         Point2D velocity = nextWaypoint.subtract(entity.getPosition())
                 .normalize()
                 .multiply(speed);
-
         entity.translate(velocity);
-
         if (nextWaypoint.distance(entity.getPosition()) < speed) {
             entity.setPosition(nextWaypoint);
             walkingAnimation();
@@ -124,7 +142,8 @@ public class EnemyComponent extends Component {
                 Random rng = new Random();
                 PropertyMap vars = FXGL.getWorldProperties();
                 if (vars.getInt("baseHealth") > 0) {
-                    vars.setValue("baseHealth", vars.getInt("baseHealth") - rng.nextInt(15, 50));
+                    vars.setValue("baseHealth",
+                            vars.getInt("baseHealth") - rng.nextInt(lowerBoundNumber, upperBoundNumber));
                 }
                 entity.removeFromWorld();
             }
